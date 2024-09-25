f4a14809906c6dbcbf700271b2f743812a1be037
Aleksander Ontin <aleksander.ontin@gmail.com>
2024-09-25 21:34:01 +0300

    The form submit event handling was added to the script file.

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+const { response } = require("express");
+
 // Create constant variables by getting elements from the HTML page
 const form = document.getElementById('crawlForm');
 const urlInput = document.getElementById('urlInput');
-const resultContainer = document.getElementById('results');
+const resultsContainer = document.getElementById('results');
 
+// Handle the form submit event
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // This function prevents the page from reloading
+
+    const url = urlInput.value;
+
+    if (!url) { // If this is not a URL link, then this condition is triggered
+        resultsContainer.innerHTML = "<p style='color: red;'>Palun sisestage URL!</p>"
+        return;
+    }
+
+    resultsContainer.innerHTML = "<p>Analüüs toimub... Palun oodake.</p>";
+
+    fetch('/api/crawl', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ url: url})
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Siin näitame varsti midagi!")
+        resultsContainer.innerHTML = "<p>Siin näitame varsti midagi!</p>";
+    })
+    .catch(error => {
+        console.error('Tekkis viga: ', error);
+        resultsContainer.innerHTML = "<p style='color: red;'>Tekkis viga. Palun proovi uuesti.</p>";
+    });
+});
