7a2666638121cda05f663c15f0ac28e8dec4429e
Aleksander Ontin <aleksander.ontin@gmail.com>
2024-09-25 21:08:34 +0300

    Constant variables created in the script file

---
+++ script.js
@@ -0,0 +1,5 @@
+// Create constant variables by getting elements from the HTML page
+const form = document.getElementById('crawlForm');
+const urlInput = document.getElementById('urlInput');
+const resultContainer = document.getElementById('results');
+
